7ae305d02f9815070319da1e7531a78fa76116ab
Tomas Vik <tvik@gitlab.com>
2020-05-10 15:01:32 +0200

    versoin

--- godu.rb
+++ godu.rb
@@ -3,6 +3,7 @@ class Godu < Formula
     homepage "https://github.com/viktomas/godu"
     
     
+    version "1.3.0"
     url "https://github.com/viktomas/godu/releases/download/v1.3/godu-darwin-amd64-1.3.tgz", :using => :curl
     sha256 "e7f3bda06cf2a557af0d65d84ecb1a149594162575ec37697bfc3de87405c798"
   
