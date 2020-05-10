508b6f19954a2a79a45d3fd9ec3653662ef1f268
Tomas Vik <tvik@gitlab.com>
2020-05-10 14:51:50 +0200

    adding a sha

--- godu.rb
+++ godu.rb
@@ -1,11 +1,13 @@
 class Godu < Formula
     desc "Simple golang utility helping to discover large files/folders."
     homepage "https://github.com/viktomas/godu"
-  
-  
+    
+    
     url "https://github.com/viktomas/godu/releases/download/v1.3/godu-darwin-amd64-1.3.tgz", :using => :curl
+    sha "e7f3bda06cf2a557af0d65d84ecb1a149594162575ec37697bfc3de87405c798"
   
     def install
       bin.install "godu"
     end
   end
+  
